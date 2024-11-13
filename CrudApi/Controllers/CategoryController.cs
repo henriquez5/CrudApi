d59e81a3b0e41d1f61d36f22fed79a9799444c67
Henrique Jorge <henrique._jorge@hotmail.com>
2024-11-13 16:35:05 -0300

    adiçao de controller

---
+++ CrudApi/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly CrudDbContext _dbContext;
+
+        public CategoryController(CrudDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("obter-todos")]
+        public async Task<IActionResult> ObterTodasCategorias()
+        {
+            try
+            {
+                var result = _dbContext.Categories.ToList();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { mensagem = "Erro interno ao processar a solicitação.", detalhe = ex.Message });
+            }
+        }
+    }
+}
